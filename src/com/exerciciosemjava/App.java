18b9958aacd378b9ebf7edfb9f6a4d4746b15394
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2020-08-15 18:56:32 -0300

    Adicionado um exercicio para descobrir se a palavra é palindroma ou não

---
+++ src/com/exerciciosemjava/App.java
@@ -0,0 +1,30 @@
+package com.exerciciosemjava;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe um nome:");
+		String nome = scan.next();
+		verificarPalindromo(nome);
+
+	}
+	
+	public static void verificarPalindromo(String nome) {
+		 String str = new String(nome);
+	     char[] array = str.toCharArray();
+	     for(int i = 0; i < array.length; i++) {
+	    	 for(int j = array.length-1; j >= 0; j--) {
+	    		 if(array[i] != array[j]) {
+	    			 System.out.printf("A palavra %s informada não é um palindromo.", str);
+	    			 System.exit(0);
+	    		 }
+	    		 i++;
+		     }
+	     }
+	     System.out.printf("A palavra %s é um palindromo.", str);
+	}
+}
